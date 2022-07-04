2c50c509e2715e5d82e455c7582bb8bbcab6bafe
Zeenat <zeenatavontuur@gmail.com>
2022-07-04 16:36:45 +0200

    initial commit

---
+++ greet.js
@@ -0,0 +1,10 @@
+/*function greet(neighbor){
+return "Hello, " + neighbor
+}
+
+var msg = greet('Xola')
+console.log(msg)*/
+
+module.exports = function(name){
+    return 'Hello, ' + name;
+}
---
+++ greet_xola.js
@@ -0,0 +1,12 @@
+//import the greet module that is in the current folder
+// const greet = require('./greet');
+
+// console.log(greet('Xola'))
+
+
+const chalk = require('chalk');
+//import the greet module that is in the current folder
+const greet = require('./greet');
+
+const styledMessage = chalk.bgWhite.black(greet('Xola'));
+console.log(styledMessage)
